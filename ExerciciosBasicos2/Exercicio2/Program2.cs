cfe656f39dc05d105e945d548c4de725b174427a
TataDevWitcher <vitorialadyletal@gmail.com>
2025-04-06 00:50:23 -0300

    Exercício 2

---
+++ ExerciciosBasicos2/Exercicio2/Program2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio2
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*Números inteiros - Par ou ímpar?*");
+
+            int numero;
+            Console.WriteLine("Insira um número inteiro e descubra o que ele é: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if(numero%2 == 0)
+            {
+                Console.WriteLine($"O número {numero} é Par!");
+            }
+            else
+            {
+                Console.WriteLine($"O número {numero} é Ímpar!");
+            }
+        }
+    }
+}
