0662727345b676cfe7ebf9017d361f82ec9fbe2a
TataDevWitcher <vitorialadyletal@gmail.com>
2025-04-05 21:45:36 -0300

    Exercício 1 - Condicional if

---
+++ ExerciciosBasicos2/Exercicio1/Program1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio1
+{
+    class Program1
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("*Números inteiros - Negativo ou positivo?*");
+
+            int numeroInt;
+
+            Console.WriteLine("Informe um número inteiro e saberá se é positivo ou negativo:");
+            numeroInt = int.Parse(Console.ReadLine());
+
+            if(numeroInt < 0)
+            {
+                Console.WriteLine($"O número {numeroInt} é negativo!");
+            }
+            else if(numeroInt > 0)
+            {
+                Console.WriteLine($"O número {numeroInt} é positvo!");
+            }
+            else
+            {
+                Console.WriteLine($"O número {numeroInt} é neutro!");
+            }
+        }
+    }
+
+}
